81e2d02334165769949b97fbd3c3658f99c08769
mzw4 <mzw4@cornell.edu>
2015-04-18 17:46:32 -0400

    working on submit review

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request
-from pymongo import MongoClient
+import pymongo
+from pymongo import MongoClient, ReturnDocument
 
 app = Flask(__name__)
 
@@ -15,44 +16,111 @@ MAX_QUERY_LENGTH = 100
 
 @app.route("/")
 def main():
+  get_dish_data()
   return render_template('main.html')
 
 # dishes and corresponding restaurants
 @app.route("/ajax_get_dish_data", methods=['GET'])
 def get_dish_data():
-  dish = request.args.get('dish', '')
-  sort_by = request.args.get('sort_by', '')
-  sort_dir = request.args.get('sort_dir', '')
+  if request.method == 'POST':
 
-  dishes = get_db_collection('dishes')
-  dishes_list = []
-  if sort_by == 'rating':
-    dishes_list = dishes.find({ name: dish }).sort({ rating: -1 if sort_dir == 'desc' else 1 }).limit(MAX_QUERY_LENGTH)
-  elif sort_by == 'price':
-    dishes_list = dishes.find({ name: dish }).sort({ price: -1 if sort_dir == 'desc' else 1 }).limit(MAX_QUERY_LENGTH)
-  elif sort_by == 'distance':
-    # do later
-    pass
+    # dish = request.args.get('dish', '')
+    # sort_by = request.args.get('sort_by', '')
+    # sort_dir = request.args.get('sort_dir', '')
 
-  # for associated restaurant data
-  restaurant_list = []
+    dish = '3-dish'
+    sort_by = 'rating' # price, distance
+    sort_dir = 'desc'
 
+    # query for dishes
+    dishes = get_db_collection('dishes')
+    dishes_list = dishes.find({ 'name': dish })\
+      .sort(sort_by,  (pymongo.DESCENDING if sort_dir == 'desc' else pymongo.ASCENDING))\
+      .limit(MAX_QUERY_LENGTH)
 
-  return jsonify(dishes_list)
+    # get associated restaurant data
+    restaurant_ids = map(lambda dish: dish['restaurant_id'], dishes_list)
+    restaurants = get_db_collection('restaurants')
+    restaurant_list = restaurants.find({'_id': {'$in': restaurant_ids}});
+
+    result = {
+      'dishes': dishes_list,
+      'restaurants': restaurant_list,
+    }
+    return result
+  else:
+    return 'failed'
 
 @app.route("/ajax_submit_review", methods=['POST'])
 def submit_review():
-  if request.method == 'POST':
-    user_phone = request.form['phone']
-    user_name = request.form['name']
-    user_friends = request.form['friends'] #list
+  # if request.method == 'POST':
+  # dish = request.args.get('dish', '')
+  # restaurant = request.args.get('restaurant', '')
+  # rating = request.args.get('rating', '')
+  # description = request.args.get('description', '')
+  # user_id = request.args.get('user_id', '')
+  # price = request.args.get('price', '')
+  # tags = request.args.get('tags', '')
+  # photo = request.args.get('photo', '')
+  # date = request.args.get('date', '')
 
-    user = {
-        'phone' : user_phone,
-        'name' : user_name,
-        'friends' : user_friends
+  dish = '5-dish'
+  restaurant = '5r'
+  review_text = 'omgomgomg'
+  rating = 5.0
+  user_id = 9000
+  price = 9999
+  tags = ['tasty', 'flaming hot']
+  photo = 'eadawd'
+
+  dishes = get_db_collection('dishes')
+  restaurants = get_db_collection('restaurants')
+  reviews = get_db_collection('reviews')
+
+  reviewed_restaurant = restaurants.find_one({ 'name': restaurant })
+  if reviewed_restaurant:
+    reviewed_restaurant_id = reviewed_restaurant['_id']
+    new_review = {
+      '_id': next_id('reviews'),
+      'user_id': user_id,
+      'rating': rating,
+      'text': review_text,
+      'photo': photo,
+      'votes': 0,
     }
-    user_mongo_id = users.insert(user)
+    new_review_id = reviews.insert(new_review)
+
+    dish_id = { 'name': dish, 'restaurant_id': reviewed_restaurant_id }
+    reviewed_dish = dishes.find_one(dish_id)
+    if reviewed_dish:
+      # update the dish with the new_review
+      print reviewed_dish
+      print dishes.update_one(dish_id,
+        {
+          '$push': {
+            'reviews': new_review_id,            # add new_review
+            'tags': { '$each': tags }           # add tags
+          },
+          '$inc': { 'num_ratings': 1 },       # increment num_ratings values
+          '$set': {                           # update running average of rating
+            'rating': (reviewed_dish['rating'] * reviewed_dish['num_ratings'] + rating) / (reviewed_dish['num_ratings'] + 1)
+          }
+        },
+      )
+    else:
+      # add the dish
+      dishes.insert({
+        '_id': next_id('dishes'),
+        'name': dish,
+        'restaurant_id': reviewed_restaurant_id,
+        'price': price,
+        'reviews': [new_review],
+        'rating': rating,
+        'num_ratings': 1,
+        'tags': tags
+      })
+
+  print dishes.find_one({ 'name': dish })
   return 'success'
 
 ###############################################################################
@@ -66,24 +134,50 @@ def get_db_connection(db):
 def get_db_collection(collection):
     return get_db_connection('brick')[collection]
 
+def next_id(collection):
+  counters = get_db_collection('counters')
+  result = counters.find_one_and_update(
+    { '_id': collection },
+    { '$inc': { 'seq': 1 } },
+    upsert=True,
+    return_document=ReturnDocument.AFTER
+  )
+  return result['seq'];
+
 def populate_mock_db():
+  get_db_collection('dishes').remove()
+  get_db_collection('restaurants').remove()
+
   dishes = get_db_collection('dishes')
-  for i in range(10):
+  for i in range(100):
     dish = {
-      'name': str(i) + '-dish',
+      '_id': i,
+      'name': str(i%10) + '-dish',
       'price': 1,
       'rating': 4.5,
       'num_ratings': 100,
-      'restaurant_id': 123,
+      'restaurant_id': i%15,
+      'reviews': [],
+      'tags': [],
     }
     dishes.insert_one(dish)
-  # for dish in dishes.find():
-  #   print dish
+
+  restaurants = get_db_collection('restaurants')
+  for i in range(15):
+    r = {
+      '_id': i,
+      'name': str(i) + 'r',
+      'address': 'crazyshit',
+      'rating': 4.5,
+      'type': ['Mexican'],
+    }
+    restaurants.insert_one(r)
 
 ###############################################################################
 ###############################     Main      ################################
 ###############################################################################
+populate_mock_db()
+submit_review()
 
 if __name__ == "__main__":
-    populate_mock_db()
-    app.run()
+    app.run(port=5000)
